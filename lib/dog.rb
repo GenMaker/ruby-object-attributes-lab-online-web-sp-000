66430f74b9b57e92b998f3ae648c0d3b1a0f884f
Panela Leung <leungpanela@gmail.com>
2019-08-30 03:56:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,6 @@
 #You'll be teaching Dog about their names through two methods, #name, and #name= that read and write to a corresponding instance variable @name.
 class Dog
-  def initize (dog_name)
+  def name= (dog_name)
     @name = dog_name
   end
 
